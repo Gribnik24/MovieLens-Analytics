1d04a625cb6eca4f8e6bb18688e226c1c8e64690
Nikita Armbrister <nikritus@gmail.com>
2026-02-27 14:33:46 +0700

    Autotests for Movies and Tags classes adding

--- src/analytics_classes_tests.py
+++ src/analytics_classes_tests.py
@@ -4,11 +4,119 @@ import pytest
 
 class Tests:
     def __init__(self):
-        self.ratings = Ratings('../data/ratings.csv')
+        self.movies = Movies('data/movies.csv')
+        self.tags = Tags('data/tags.csv')
+        self.ratings = Ratings('data/ratings.csv')
         self.links = Links('data/links.csv')
+
+    def movies_tests(self):
+        assert len(self.movies.movies) == 1000
+
+        dist_by_release_result = self.movies.dist_by_release()
+        assert type(dist_by_release_result).__name__ == 'dict'
+
+        for value in dist_by_release_result.values():
+           assert type(value).__name__ == 'int'
         
+        values = list(dist_by_release_result.values())
+        assert values == sorted(values, reverse=True)
+
+        dist_by_genres_result = self.movies.dist_by_genres()
+        assert type(dist_by_genres_result).__name__ == 'dict'
+
+        for value in dist_by_release_result.values():
+           assert type(value).__name__ == 'int'
+
+        values = list(dist_by_genres_result.values())
+        assert values == sorted(values, reverse=True)
+
+
+        dist_most_genres = self.movies.most_genres(10)
+        assert type(dist_most_genres).__name__ == 'dict'
+        assert len(dist_most_genres) == 10
+
+        values = list(dist_most_genres.values())
+        assert values == sorted(values, reverse=True)
+
+        with pytest.raises(TypeError):
+            self.movies.most_genres()
+
+       
+        empty_result = self.movies.most_genres(0)
+        assert isinstance(empty_result, dict)
+        assert len(empty_result) == 0
+        
+      
+        for movie in self.movies.movies:
+            assert 'title' in movie
+            assert 'genres' in movie
+            assert 'id' in movie
+
+    def tags_tests(self):
+        assert len(self.tags.tags) == 1000
+
+        for tag_item in self.tags.tags:
+            assert 'user_id' in tag_item
+            assert 'movie_id' in tag_item
+            assert 'tag' in tag_item
+            assert 'timestamp' in tag_item
+
+        dist_most_words = self.tags.most_words(10)
+        assert type(dist_most_words).__name__ == 'dict'
+        assert len(dist_most_words) == 10
+   
+        for value in dist_most_words.values():
+            assert type(value).__name__ == 'int'
+  
+        values = list(dist_most_words.values())
+        assert values == sorted(values, reverse=True)
+
+        list_longest = self.tags.longest(10)
+        assert type(list_longest).__name__ == 'list'
+        assert len(list_longest) == 10
+        for item in list_longest:
+            assert type(item).__name__ == 'str'
+
+        lengths = [len(item) for item in list_longest]
+        assert lengths == sorted(lengths, reverse=True)
+
+        list_most_words_and_longest = self.tags.most_words_and_longest(10)
+        assert type(list_most_words_and_longest).__name__ == 'list'
+        assert len(list_most_words_and_longest) <= 10
+        for item in list_most_words_and_longest:
+            assert type(item).__name__ == 'str'
+        assert list_most_words_and_longest == sorted(list_most_words_and_longest)
+
+        dist_most_popular = self.tags.most_popular(10)
+        assert type(dist_most_popular).__name__ == 'dict'
+        assert len(dist_most_popular) == 10
+     
+        for value in dist_most_popular.values():
+            assert type(value).__name__ == 'int'
+
+        values = list(dist_most_popular.values())
+        assert values == sorted(values, reverse=True)
+
+        tags_with_result = self.tags.tags_with("classic")
+        assert type(tags_with_result).__name__ == 'list'
+        for item in tags_with_result:
+            assert type(item).__name__ == 'str'
+        assert tags_with_result == sorted(tags_with_result)
+
+        with pytest.raises(TypeError):
+            self.tags.most_words("10")
+        
+        with pytest.raises(TypeError):
+            self.tags.longest("10")
+            
+        with pytest.raises(TypeError):
+            self.tags.most_popular("10")
+            
+        with pytest.raises(TypeError):
+            self.tags.tags_with(123)
+            
     def ratings_tests(self):
-        assert len(self.ratings.ratings_csv) == 1000 # Checking the ratings_csv containing 1000 rows
+        assert len(self.ratings.ratings_csv) == 1000
         
         assert self.ratings.find_title_by_id('1') == 'Toy Story (1995)'
         assert self.ratings.find_title_by_id('4654') == 'Road House (1989)'
